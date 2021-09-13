1f80f6f6e8fa90d8acedc4c40ead287bd32715fd
Ubuntu <ubuntu@ip-172-31-51-232.ec2.internal>
2021-09-13 17:51:50 +0000

    added file

---
+++ area_sphere.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by: Seti Ngabo
+# Created on: Sept 2021
+# this program calculates the area and perimeter of a rectangle
+#   with dimensions 5cm x 3cm
+
+
+import math
+
+
+def main():
+    # this function calculates the area and perimeter
+
+    print("If a circle has a radius of 15mm:")
+    print("")
+    print("Area is {}mm^2.".format(math.pi * 15 ** 2))
+    print("Perimeter is {}mm.".format(2 * math.pi * 15))
+    print("")
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
